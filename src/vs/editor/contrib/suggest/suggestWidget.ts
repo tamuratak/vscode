27d87aa489686ab17e17f6addaf2c61c2f7c30a6
Takashi Tamura <tamuratak@users.noreply.github.com>
2019-02-25 14:50:05 +0900

    set a larger maxHeight for the detail of completionItem

--- src/vs/editor/contrib/suggest/suggestWidget.ts
+++ src/vs/editor/contrib/suggest/suggestWidget.ts
@@ -1100,6 +1100,7 @@ export class SuggestWidget implements IContentWidget, IListVirtualDelegate<Compl
 			// Docs is bigger than list and widget is above cursor, apply margin-top so that list appears right above cursor
 			this.listElement.style.marginTop = `${this.details.element.offsetHeight - this.listElement.offsetHeight}px`;
 		}
+		this.details.element.style.maxHeight = `${Math.max(this.editor.getLayoutInfo().height - this.listElement.offsetHeight - 10, this.maxWidgetHeight)}px`;
 	}
 
 	/**
